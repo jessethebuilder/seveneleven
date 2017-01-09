e5cabd1ff2197029c13c5e48fb77896bc3930bea
Jesse Farmer <jessethebuilder@yahoo.com>
2017-01-09 14:57:48 -0800

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :intl_stores
   resources :na_stores
+
+  root to: 'na_stores#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
