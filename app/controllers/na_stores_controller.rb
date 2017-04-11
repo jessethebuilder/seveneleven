3983b95c7bd37da8ad02b18e558db59c2ea9a43e
Jesse Farmer <jessethebuilder@yahoo.com>
2017-04-11 15:31:24 -0700

    123

--- app/controllers/na_stores_controller.rb
+++ app/controllers/na_stores_controller.rb
@@ -53,7 +53,7 @@ class NaStoresController < ApplicationController
     filter_terms = params[:filter_terms]
 
     # count = NaStore.count
-    count = 5
+    count = 100
 
     if(filter_bys && filter_terms)
       bys = filter_bys.split(',')
